7b67cb02434da3421ba1a2716f5fb053d5b8d042
acmerfight <acmerfight@gmail.com>
2016-01-06 23:56:39 +0800

    fix js bind bug

--- DOM_script/scripts/showPic.js
+++ DOM_script/scripts/showPic.js
@@ -14,8 +14,10 @@ function prepareLinks() {
     var links = document.getElementsByTagName("a")
     for (var i=0; i<links.length; i++) {
         if (links[i].getAttribute("class") == "popup") {
-            popUp(this.getAttribute("href"))
-            return false
+            links[i].onclick = function() {
+                popUp(this.getAttribute("href"))
+                return false
+            }
         }
     }
 }
