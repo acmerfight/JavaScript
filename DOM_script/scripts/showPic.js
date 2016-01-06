537e12af8dcab3fb58d458a4f45687a26a13f02b
acmerfight <acmerfight@gmail.com>
2016-01-06 23:50:51 +0800

    serperate js

--- DOM_script/scripts/showPic.js
+++ DOM_script/scripts/showPic.js
@@ -7,3 +7,19 @@ function showPic(whichpic) {
     var description = document.getElementById("description")
     description.firstChild.nodeValue = text
 }
+
+
+window.onload = prepareLinks
+function prepareLinks() {
+    var links = document.getElementsByTagName("a")
+    for (var i=0; i<links.length; i++) {
+        if (links[i].getAttribute("class") == "popup") {
+            popUp(this.getAttribute("href"))
+            return false
+        }
+    }
+}
+
+function popUp(winURL) {
+    window.open(winURL, "popup", "width=320, height=480")
+}
