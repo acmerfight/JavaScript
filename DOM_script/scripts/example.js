f7a0a68d7412b86a21f9774673d6e301d1b5213d
acmerfight <acmerfight@gmail.com>
2016-01-14 18:42:42 +0800

    add example test

---
+++ DOM_script/scripts/example.js
@@ -0,0 +1,23 @@
+
+
+addLoadEvent(insertParagraph)
+
+function addLoadEvent(func) {
+    var old_load = window.onload
+    if (typeof window.onload != "function") {
+        window.onload = func
+    } else {
+        window.onload = function() {
+            old_load()
+            func()
+        }
+    }
+}
+
+
+function insertParagraph(text) {
+    var str = "<p>"
+    str += text
+    str += "</p>"
+    document.write(str)
+}
