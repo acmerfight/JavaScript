2e11a98316184c9d4146027bda248e23db2b7e1b
acmerfight <acmerfight@gmail.com>
2016-01-17 23:54:28 +0800

    add createElement

--- DOM_script/scripts/example.js
+++ DOM_script/scripts/example.js
@@ -1,4 +1,12 @@
 
+window.onload = function() {
+    var para = document.createElement("p")
+    var info = "nodeName: "
+    info += para.nodeName
+    info += " nodeType: "
+    info += para.nodeType
+    alert(info)
+}
 
 function addLoadEvent(func) {
     var old_load = window.onload
