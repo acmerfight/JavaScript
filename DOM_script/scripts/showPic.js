07adc8ff28bee1d490d815125a75254a8e0da36e
acmerfight <acmerfight@gmail.com>
2016-01-18 23:35:47 +0800

    add preparePlaceholder

--- DOM_script/scripts/showPic.js
+++ DOM_script/scripts/showPic.js
@@ -13,6 +13,24 @@ function addLoadEvent(func) {
 }
 
 
+function preparePlaceholder() {
+    if(!document.createElement) return false
+    if(!document.createTextNode) return false
+    if(!document.getElementById) return false
+    if(!document.getElementById("imagegallery")) return false
+    var placeholder = document.createElement("img")
+    placeholder.setAttribute("id", "placeholder")
+    placeholder.setAttribute("src", "images/1.jpeg")
+    placeholder.setAttribute("alt", "my image gallery")
+    var description = document.createElement("p")
+    description.setAttribute("id", "description")
+    var desctext = document.createTextNode("Choose an image")
+    description.appendChild(desctext)
+    var gallery = document.getElementById("imagegallery")
+    insertAfter(placeholder, gallery)
+    insertAfter(description, placeholder)
+}
+
 function prepareGallery() {
     if(!document.getElementsByTagName) return false
     if(!document.getElementById) return false
@@ -53,6 +71,15 @@ function prepareLinks() {
                 return false
             }
         }
+    }
+}
+
+function insertAfter(newElement, targetElement) {
+    var parent = targetElement.parentNode;
+    if (parent.lastChild = targetElement) {
+        parent.appendChild(newElement)
+    } else {
+        parent.insertBefore(newElement, targetElement.nextSibling)
     }
 }
 
