3b34d7980a9927f7e4f1c451e2eeb56a98a796c3
acmerfight <acmerfight@gmail.com>
2016-01-24 23:51:40 +0800

    add organize array

--- DOM_script/scripts/displayAbbreviations.js
+++ DOM_script/scripts/displayAbbreviations.js
@@ -1,4 +1,17 @@
 function  displayAbbreviations() {
     var abbreviations = document.getElementsByTagName("abbr")
     if (abbreviations.length < 1) return false
+    var defs = []
+    for (var i=0; i<abbreviations.length; i++) {
+        var current_abbr = abbreviations[i]
+        var definition = current_abbr.getAttribute("title")
+        var key = current_abbr.lastChild.nodeValue
+        defs[key] = definition
+    }
+    var dlist = document.createElement("dl")
+    for (key in defs) {
+        var definition = defs[key]
+        var dtitle = document.createElement("dt")
+        var dtitle_text = document.createTextNode(key)
+    }
 }
