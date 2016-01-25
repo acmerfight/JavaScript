ff44ff47aebe398064d2a0c5671785f62bfd9410
acmerfight <acmerfight@gmail.com>
2016-01-25 23:49:57 +0800

    add displayAbbreviations

--- DOM_script/scripts/displayAbbreviations.js
+++ DOM_script/scripts/displayAbbreviations.js
@@ -13,5 +13,11 @@ function  displayAbbreviations() {
         var definition = defs[key]
         var dtitle = document.createElement("dt")
         var dtitle_text = document.createTextNode(key)
+        dtitle.appendChild(dtitle_text)
+        var ddesc = document.createElement("dd")
+        var ddesc_text = document.createTextNode(definition)
+        ddesc.appendChild(ddesc_text)
+        dlist.appendChild(dtitle)
+        dlist.appendChild(ddesc)
     }
 }
